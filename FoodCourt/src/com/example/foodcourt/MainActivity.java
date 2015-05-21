309844cca353a25fd787e92b6605106ff5669262
Ralf Nieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2015-05-21 10:10:43 +0200

    Live classification

--- FoodCourt/src/com/example/foodcourt/MainActivity.java
+++ FoodCourt/src/com/example/foodcourt/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.foodcourt;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,7 +15,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,14 +22,17 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
-	private SensorManager sensorManager;
+    private final int TIER_1_SAMPLING_TIME = 1000;
+    private SensorManager sensorManager;
 	private Sensor accelerometer;
-	private TextView test;
-	private String data;
+	private TextView currentActivityLabel;
+	private String data = "";
 	private float starttime = 0;
 	private String status = "Standing";
+    ArrayList<Instance> trainingSet = null;
+    ArrayList<Label.Activities> currentSession = null;
 
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -49,19 +51,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 			// fail! we dont have an accelerometer!
 		}
 
-		final Button clear = (Button) findViewById(R.id.clear);
-		clear.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				// Clear text view on click
-				test.setText("");
-			}
-		});
+        currentSession = new ArrayList<Label.Activities>();
+
+		try {
+			InputStream trainStream = getAssets().open("trainingSet.csv");
+			FileReader trainReader = new FileReader(trainStream);
+			trainingSet = trainReader.buildInstances();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void initializeViews() {
-
-		test = (TextView) findViewById(R.id.test);
-
+		currentActivityLabel = (TextView) findViewById(R.id.currentActivityLabel);
 	}
 
 	// onResume() register the accelerometer for listening the events
@@ -80,16 +82,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-
 		float x = event.values[0];
 		float y = event.values[1];
 		float z = event.values[2];
-		float timestamp;
 		float now, time;
 		if (starttime == 0) {
 			starttime = event.timestamp;
@@ -98,11 +97,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 		time = Math.round((now - starttime) / 1000000);
 
 		double magnitude = Math.sqrt(x * x + y * y + z * z);
-		test.append(status + "," + magnitude + "," + time + "\n");
-		data = test.getText().toString();
+		data += status + "," + magnitude + "," + time + "\n";
 
-		test.setMovementMethod(new ScrollingMovementMethod());
-
+		if (time > TIER_1_SAMPLING_TIME) {
+			currentSession.add(classify());
+			data = "";
+		}
 	}
 
 	public void startAcc(View view) {
@@ -118,11 +118,12 @@ public class MainActivity extends Activity implements SensorEventListener {
 			myFile.createNewFile();
 			FileOutputStream fOut = new FileOutputStream(myFile);
 			OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-			myOutWriter.append(test.getText());
+			myOutWriter.append(data);
 			myOutWriter.close();
 			fOut.close();
 			Toast.makeText(getBaseContext(), "Done writing file in SD",
 					Toast.LENGTH_SHORT).show();
+			System.out.println(data);
 		} catch (Exception e) {
 			Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_SHORT)
 					.show();
@@ -131,8 +132,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	}
 
 	public void change(View view) {
-
-		if (status == "Standing") {
+		if (status.equals("Standing")) {
 			status = "Walking";
 			Button p1_button = (Button) findViewById(R.id.change);
 			p1_button.setText("Walking");
@@ -141,53 +141,49 @@ public class MainActivity extends Activity implements SensorEventListener {
 			Button p1_button = (Button) findViewById(R.id.change);
 			p1_button.setText("Standing");
 		}
-
 	}
 
-	public void readfile(View view) throws IOException {
-
+	public Label.Activities classify() {
 		starttime = 0;
 
-		System.out
-				.println("KNN STUFF! ---------------------------------------");
 		ArrayList<Instance> newInstances = null;
-		ArrayList<Instance> trainInstances = null;
 		ArrayList<Neighbor> distances = null;
 		ArrayList<Neighbor> neighbors = null;
 		Label.Activities classification = null;
 		Instance classificationInstance = null;
-		FileReader newReader = null;
-		FileReader trainReader = null;
 
-		File file = new File("/sdcard/mysdfile.txt");
-		InputStream trainStream = getAssets().open("testing4.csv");
-		FileInputStream newStream = new FileInputStream(file);
-		Toast.makeText(getBaseContext(), "Reading File", Toast.LENGTH_SHORT)
-				.show();
-		newReader = new FileReader(newStream);
-		trainReader = new FileReader(trainStream);
+		FileReader newReader = new FileReader(new ByteArrayInputStream(data.getBytes()));
 		newInstances = newReader.buildInstances();
-		trainInstances = trainReader.buildInstances();
 
+		int walking = 0, standing = 0;
 		do {
 			classificationInstance = newInstances.remove(0);
 
-			distances = Knn.calculateDistances(trainInstances,
-					classificationInstance);
+			distances = Knn.calculateDistances(trainingSet,	classificationInstance);
 			neighbors = Knn.getNearestNeighbors(distances);
 			classification = Knn.determineMajority(neighbors);
 
-			// Knn.printNeighbors(neighbors);
-			System.out.println("\n-----Instance-----: ");
-			Knn.printClassificationInstance(classificationInstance);
-			System.out.println("\nExpected situation result for instance: "
-					+ classification.toString());
+			switch(classification) {
+				case Walking: walking++; break;
+				case Standing: standing++; break;
+				default: throw new IllegalArgumentException("UNKNOWN classification");
+			}
 		} while (!newInstances.isEmpty());
 
+		Label.Activities currentActivity = walking >= standing ? Label.Activities.Walking : Label.Activities.Standing;
+		currentActivityLabel.setText(currentActivity.toString());
+
+        System.out.println("Current activity: "+currentActivity.toString());
+
+        return currentActivity;
 	}
 
 	public void stopAcc(View view) {
 		sensorManager.unregisterListener(this);
 		starttime = 0;
+        System.out.println("------LIST OF CLASSIFICATIONS------");
+        for (Label.Activities label : currentSession) {
+            System.out.println(label);
+        }
 	}
 }
